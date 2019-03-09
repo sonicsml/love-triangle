cd7c4051fe10ea7f38bb52de502d85405c8ecdce
sonicsml <sonics.scs@gmail.com>
2019-03-09 03:38:12 +0300

    Update with my decision Love-triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,14 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var result = 0;
+  for (var n = 0; n < preferences.length; n++) {
+    var firstSpichonee = preferences[n] - 1; 
+    var secondSpichonee = preferences[firstSpichonee] - 1;
+    var thirdSpichonee = preferences[secondSpichonee] - 1;
+    if (thirdSpichonee == n) {
+      result++;
+    }
+  }
+  return Math.floor(result / 3);
 };
